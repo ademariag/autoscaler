20165f05131fce36baa0c165269dcd32ca98b51e
Alessandro De Maria <a.demaria@synthace.com>
2020-07-09 15:23:17 +0200

    Fix WithScopes test

--- drivers/google/option_test.go
+++ drivers/google/option_test.go
@@ -5,8 +5,10 @@
 package google
 
 import (
+  "github.com/google/go-cmp/cmp"
 	"net/http"
 	"testing"
+	"strings"
 )
 
 func TestOptions(t *testing.T) {
@@ -20,6 +22,7 @@ func TestOptions(t *testing.T) {
 		WithProject("my-project"),
 		WithTags("drone", "agent"),
 		WithZone("us-central1-f"),
+		WithScopes("scope1,scope2"),
 	)
 	if err != nil {
 		t.Error(err)
@@ -50,5 +53,8 @@ func TestOptions(t *testing.T) {
 	}
 	if got, want := p.zone, "us-central1-f"; got != want {
 		t.Errorf("Want zone %q, got %q", want, got)
+	}
+	if got, want := p.scopes, strings.Split("scope1,scope2", ","); ! cmp.Equal(got, want) {
+		t.Errorf("Want scopes %q, got %q", want, got)
 	}
 }
